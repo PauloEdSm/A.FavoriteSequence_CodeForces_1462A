540c28ec9a66e356f64737b51482ce4663a96e3b
paulodev <pauloeduardotroll9646@gmail.com>
2025-12-29 17:57:26 -0300

    Save progress

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,27 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public void main(String[] args) {
-        final int MAX_SEQUENCE_LENGTH = 100;
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int width = 0;
+        int [] elements = new int[300];
+        int insertions = scanner.nextInt();
+             for (int i =0; i< insertions;i++){
+                width = scanner.nextInt();
+                elements[0] = scanner.nextInt();
 
-        int maxElements = 0;
-        int totalSequence = scanner.nextInt();
-        int numElements = 0;
-
-        int [] currentSequence = new int[totalSequence];
-        int [][] sortedSequence = new int[totalSequence][MAX_SEQUENCE_LENGTH];
-
-        for (int j = 0; j < totalSequence ; j++) {
-            numElements = scanner.nextInt();
-            currentSequence[j] = scanner.nextInt();
-            for (int i = 0; i < numElements; i++) {
-            }
         }
-        System.out.println();
-
-
-
-
-        scanner.close();
     }
+    public int[] SortedElements (int width, int[] elements) {
+        int [] sortedElements = new int [300];
+        for (int i =0, j =elements.length -1;i < width;i++, j--) {
+            sortedElements[(i<elements.length/2-2) ? elements[i] : elements.length-1] = elements[i];
+            sortedElements[(j>elements.length/2-2) ? elements[j] : elements.length-1] = elements[j];
 
+        }
+
+        return sortedElements;
+
+    }
 }
